20d35e40b2ca4060e5938b7d466002f55bc803bf
WangShaoze <2847832624@qq.com>
2024-11-23 16:11:07 +0800

    在模块中实现单例，导入的方式获取单例对象

--- 03-single-pattern/implement_singleton_by_module/main.py
+++ 03-single-pattern/implement_singleton_by_module/main.py
@@ -1,9 +1,3 @@
-"""
-
-Python 的模块本身就是 单例模式
-
-"""
-
 from demo1 import demo1_singleton
 from demo2 import demo2_singleton
 
@@ -13,7 +7,7 @@ if __name__ == '__main__':
     s.set_b(30)
     print(s)
 
-    s = demo2_singleton
-    s.set_a(1)
-    s.set_b(3)
-    print(s)
+    s1 = demo2_singleton
+    s1.set_a(1)
+    s1.set_b(3)
+    print(s1)
